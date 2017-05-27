70ce931c3903ee8ce7c3634b0c0405332e843f40
Tonny Katongole <tonny.katongole@gmail.com>
2017-05-27 13:51:08 +0300

    Added data fixtures and updated readme

---
+++ src/AppBundle/DataFixtures/ORM/LoadUserData.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * @author Tonny Katongole <tonny.katongole@gmail.com>
+ */
+
+namespace AppBundle\DataFixtures\ORM;
+
+use Doctrine\Common\DataFixtures\FixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+use AppBundle\Entity\User;
+
+class LoadUserData implements FixtureInterface
+{
+    public function load(ObjectManager $manager)
+    {
+        $userAdmin = new User();
+        $userAdmin->setUsername('admin');
+        $userAdmin->setFirstName('Admin');
+        $userAdmin->setOtherNames('Admin');
+        $userAdmin->setComicName('Zizinga');
+        $userAdmin->setPassword('admin');
+        $userAdmin->setEnabled(true);
+        $userAdmin->setEmail('admin@zizinga.me');
+        $userAdmin->addRole('ROLE_ADMIN');
+        $manager->persist($userAdmin);
+        $manager->flush();
+    }
+}
