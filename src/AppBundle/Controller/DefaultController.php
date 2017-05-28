f70602579d8fccbb7427c50f567780c2379bb247
Tonny Katongole <tonny.katongole@gmail.com>
2017-05-28 09:21:38 +0300

    Fixed layout and some issues with registration
    -Overriding the FOSUserBundle with custom fields and logic

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -13,9 +13,7 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
-        // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
-        ]);
+        //Go to the Jokes Index
+        return $this->redirectToRoute('joke_index');
     }
 }
